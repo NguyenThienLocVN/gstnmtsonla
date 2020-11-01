787429962ddf7ed30bf492013bf2cecf8f461791
Loc Nguyen <kten1112@gmail.com>
2020-11-01 11:52:33 +0700

    #4 Add local bootstrap & style navigation for desktop

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,10 +1,23 @@
 <nav class="main-navigation">
-    <div class="float-left text-center font-weight-bold"><a href="#">GIỚI THIỆU CHUNG</a></div>
-    <div class="float-left text-center font-weight-bold"><a href="#">QUAN TRẮC</a></div>
-    <div class="float-left text-center font-weight-bold"><a href="#">GIÁM SÁT</a></div>
-    <div class="float-left text-center font-weight-bold"><a href="#">CẤP PHÉP</a></div>
-    <div class="float-left text-center font-weight-bold"><a href="#">THÔNG TIN DỮ LIỆU</a></div>
-    <div class="float-left text-center font-weight-bold"><a href="#">THÔNG BÁO</a></div>
-    <div class="float-left text-center font-weight-bold"><a href="#">BÁO CÁO</a></div>
-    <div class="float-left text-center font-weight-bold"><a href="#">ĐĂNG KÝ</a></div>
+    <div class="navigation-item float-left text-center font-weight-bold"><a href="#">GIỚI THIỆU CHUNG</a></div>
+    <div class="navigation-item float-left text-center font-weight-bold"><a href="#">QUAN TRẮC</a></div>
+    <div class="navigation-item float-left text-center font-weight-bold position-relative"><a href="#">GIÁM SÁT</a>
+        <div class="sub-menu position-absolute text-left">
+            <div><a href="#">Hồ chứa thủy điện trên 2MW</a></div>
+            <div><a href="#">Hồ chứa thủy điện dưới 2MW</a></div>
+            <div><a href="#">Hồ chứa khai thác nước mặt cho sản xuất</a></div>
+            <div><a href="#">Trạm bơm</a></div>
+            <div><a href="#">Khai thác, sử dụng nước dưới đất</a></div>
+        </div>
+    </div>
+    <div class="navigation-item float-left text-center font-weight-bold"><a href="#">CẤP PHÉP</a>
+        <div class="sub-menu position-absolute text-left">
+            <div><a href="#">QLCP khai thác sử dụng nước mặt </a></div>
+            <div><a href="#">QLCP khai thác sử dụng nước dưới đất</a></div>
+        </div>
+    </div>
+    <div class="navigation-item float-left text-center font-weight-bold"><a href="#">THÔNG TIN DỮ LIỆU</a></div>
+    <div class="navigation-item float-left text-center font-weight-bold"><a href="#">THÔNG BÁO</a></div>
+    <div class="navigation-item float-left text-center font-weight-bold"><a href="#">BÁO CÁO</a></div>
+    <div class="navigation-item float-left text-center font-weight-bold"><a href="#">ĐĂNG KÝ</a></div>
 </nav>
--- resources/views/pages/tai-nguyen-nuoc.blade.php
+++ resources/views/pages/tai-nguyen-nuoc.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <main class="main-welcom container-fluid p-0 position-relative">
-    <div class="text-center w-100">
+    <div class="text-center w-100 bg-white">
         <h5 class="text-primary p-2 font-weight-bold">HỆ THỐNG QUẢN LÝ, GIÁM SÁT, KHAI THÁC SỬ DỤNG  TÀI NGUYÊN NƯỚC - TỈNH SƠN LA</h5>
     </div>
     @include('layouts.navigation')
