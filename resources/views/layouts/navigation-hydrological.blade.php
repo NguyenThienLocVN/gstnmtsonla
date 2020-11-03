4fc66a0747f5ffbe9d17ed0600782e54bca2c72b
Loc Nguyen <kten1112@gmail.com>
2020-11-03 20:46:43 +0700

    #11 Add submenu

--- resources/views/layouts/navigation-hydrological.blade.php
+++ resources/views/layouts/navigation-hydrological.blade.php
@@ -1,11 +1,45 @@
 <nav class="main-navigation">
     <div class="hd-navigation-item float-left text-center font-weight-bold"><a href="#">GIỚI THIỆU</a></div>
     <div class="hd-navigation-item float-left text-center font-weight-bold"><a href="#">QUAN TRẮC</a></div>
-    <div class="hd-navigation-item float-left text-center font-weight-bold"><a href="#">DỰ BÁO</a></div>
-    <div class="hd-navigation-item float-left text-center font-weight-bold"><a href="#">HỒ SƠ</a></div>
-    <div class="hd-navigation-item float-left text-center font-weight-bold"><a href="#">BIẾN ĐỔI KHÍ HẬU</a></div>
-    <div class="hd-navigation-item float-left text-center font-weight-bold"><a href="#">CHƯƠNG TRÌNH</a></div>
-    <div class="hd-navigation-item float-left text-center font-weight-bold"><a href="#">VĂN BẢN</a></div>
+    <div class="hd-navigation-item float-left text-center font-weight-bold"><a href="#">DỰ BÁO</a>
+        <div class="sub-menu position-absolute text-left">
+            <div><a href="#">Bản tin dự báo thời tiết, thủy văn hàng ngày </a></div>
+            <div><a href="#">Bản tin dự báo khí tượng thủy văn 10 ngày </a></div>
+            <div><a href="#">Tuần báo khí tượng </a></div>
+            <div><a href="#">Thông báo khí tượng tháng </a></div>
+            <div><a href="#">Bản tin dự báo bão, Áp thấp nhiệt đới </a></div>
+            <div><a href="#">Bản tin dự báo, cảnh báo mưa giông </a></div>
+            <div><a href="#">Bản tin dự báo, cảnh báo mưa lớn </a></div>
+            <div><a href="#">Bản tin dự báo, cảnh báo lũ, lũ quét và sạt lở </a></div>
+            <div><a href="#">Bản tin dự báo, cảnh báo rét, rét đậm, rét hại </a></div>
+        </div>
+    </div>
+    <div class="hd-navigation-item float-left text-center font-weight-bold"><a href="#">HỒ SƠ</a>
+        <div class="sub-menu position-absolute text-left">
+            <div><a href="#">Cấp giấy phép hoạt động dự báo, cảnh báo KTTV</a></div>
+            <div><a href="#">Cấp phép gia hạn, sửa đổi, bổ sung giấy phép hoạt động dự báo, cảnh báo KTTV</a></div>
+            <div><a href="#">Cấp lại giấy phép hoạt động dự báo, cảnh báo KTTV </a></div>
+        </div>
+    </div>
+    <div class="hd-navigation-item float-left text-center font-weight-bold"><a href="#">BIẾN ĐỔI KHÍ HẬU</a>
+        <div class="sub-menu position-absolute text-left">
+            <div><a href="#">Kết quả đánh giá khí hậu quốc gia</a></div>
+            <div><a href="#">Kịch bản biến đổi khí hậu chi tiết cho tỉnh Sơn La</a></div>
+        </div>
+    </div>
+    <div class="hd-navigation-item float-left text-center font-weight-bold"><a href="#">CHƯƠNG TRÌNH</a>
+        <div class="sub-menu position-absolute text-left">
+            <div><a href="#">Chương trình, kế hoạch, quy hoạch về khí tượng thủy văn </a></div>
+            <div><a href="#">Các dự án về khí tượng thủy văn </a></div>
+        </div>
+    </div>
+    <div class="hd-navigation-item float-left text-center font-weight-bold"><a href="#">VĂN BẢN</a>
+        <div class="sub-menu position-absolute text-left">
+            <div><a href="#">Do Quốc hội, Chính phủ, Thủ tướng chính phủ ban hành </a></div>
+            <div><a href="#">Do Bộ TNMT và các Bộ có liên quan ban hành </a></div>
+            <div><a href="#">Do UBND tỉnh ban hành</a></div>
+        </div>
+    </div>
     <div class="hd-navigation-item float-left text-center font-weight-bold"><a href="#">BÁO CÁO</a></div>
     <div class="hd-navigation-item float-left text-center font-weight-bold"><a href="#">ĐĂNG KÝ</a></div>
 </nav>
--- resources/views/layouts/navigation-water-resource.blade.php
+++ resources/views/layouts/navigation-water-resource.blade.php
@@ -1,10 +1,34 @@
 <nav class="main-navigation">
     <div class="wr-navigation-item float-left text-center font-weight-bold"><a href="#">GIỚI THIỆU</a></div>
-    <div class="wr-navigation-item float-left text-center font-weight-bold"><a href="#">QUAN TRẮC</a></div>
-    <div class="wr-navigation-item float-left text-center font-weight-bold"><a href="#">GIÁM SÁT</a></div>
-    <div class="wr-navigation-item float-left text-center font-weight-bold"><a href="#">CẤP PHÉP</a></div>
-    <div class="wr-navigation-item float-left text-center font-weight-bold"><a href="#">THÔNG TIN DỮ LIỆU</a></div>
-    <div class="wr-navigation-item float-left text-center font-weight-bold"><a href="#">THÔNG BÁO</a></div>
+    <div class="wr-navigation-item float-left text-center font-weight-bold"><a href="#">GIÁM SÁT</a>
+        <div class="sub-menu position-absolute text-left">
+            <div><a href="#">Hồ chứa thủy điện trên 2MW </a></div>
+            <div><a href="#">Hồ chứa thủy điện dưới 2MW </a></div>
+            <div><a href="#">Hồ chứa khai thác nước mặt sản xuất nông nghiệp </a></div>
+            <div><a href="#">Trạm bơm </a></div>
+            <div><a href="#">Công trình khai thác, sử dụng nước dưới đất </a></div>
+        </div>
+    </div>
+    <div class="wr-navigation-item float-left text-center font-weight-bold"><a href="#">CẤP PHÉP</a>
+        <div class="sub-menu position-absolute text-left">
+            <div><a href="#">QLCP khai thác sử dụng nước mặt </a></div>
+            <div><a href="#">QLCP khai thác sử dụng nước dưới đất </a></div>
+        </div>
+    </div>
+    <div class="wr-navigation-item float-left text-center font-weight-bold"><a href="#">THÔNG TIN DỮ LIỆU</a>
+        <div class="sub-menu position-absolute text-left">
+            <div><a href="#">Tiểu vùng TNN </a></div>
+            <div><a href="#">Số lượng, chất lượng nước mặt, nước dưới đất </a></div>
+            <div><a href="#">Số liệu điều tra khảo sát địa chất thủy văn </a></div>
+            <div><a href="#">Kết quả cấp, gia hạn, thu hồi, điều chỉnh giấy phép </a></div>
+        </div>
+    </div>
+    <div class="wr-navigation-item float-left text-center font-weight-bold"><a href="#">THÔNG BÁO</a>
+        <div class="sub-menu position-absolute text-left">
+            <div><a href="#">Tổ chức/ cá nhân </a></div>
+            <div><a href="#">Cơ quan nhà nước  </a></div>
+        </div>
+    </div>
     <div class="wr-navigation-item float-left text-center font-weight-bold"><a href="#">BÁO CÁO</a></div>
     <div class="wr-navigation-item float-left text-center font-weight-bold"><a href="#">ĐĂNG KÝ</a></div>
 </nav>
