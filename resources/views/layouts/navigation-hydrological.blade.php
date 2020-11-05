975b986c34deee9957771d2984e62ffc5e3d54df
githubnguyendang <thienphong440011@gmail.com>
2020-11-05 17:38:15 +0700

    #18 Add drop-down menu

--- resources/views/layouts/navigation-hydrological.blade.php
+++ resources/views/layouts/navigation-hydrological.blade.php
@@ -1,66 +1,63 @@
-<nav class="navbar navbar-expand-lg main-navigation nav-height navbar-dark p-0">
-    <button class="navbar-toggler p-0 ml-2" data-target="#my-nav" data-toggle="collapse" aria-controls="my-nav" aria-expanded="false" aria-label="Toggle navigation">
-        <a class="d-block"><i class="fa fa-align-justify" aria-hidden="true"></i></a>
-    </button>
-    <div id="my-nav" class="collapse navbar-collapse">
-        <ul class="navbar-nav justify-content-between nav-height w-100 text-center">
-            <li class="hd-navigation-item float-left text-left font-weight-bold">
-                <a href="#" class="nav-link px-1 py-0">GIỚI THIỆU</a>
-            </li>
-            <li class="hd-navigation-item float-left text-left font-weight-bold">
-                <a href="#" class="nav-link px-1 py-0">QUAN TRẮC</a>
-                <div class="sub-menu position-absolute text-left">
-                    <div><a href="{{ route('so-lieu-khi-tuong') }}">Số liệu khí tượng </a></div>
-                    <div><a href="#">Số liệu mưa </a></div>
-                    <div><a href="#">Số liệu thủy văn </a></div>
-                </div>
-            </li>
-            <li class="hd-navigation-item float-left text-left font-weight-bold"><a href="#" class="nav-link px-1 py-0">DỰ BÁO</a>
-                <div class="sub-menu position-absolute text-left">
-                    <div><a href="#">Bản tin dự báo thời tiết, thủy văn hàng ngày </a></div>
-                    <div><a href="#">Bản tin dự báo khí tượng thủy văn 10 ngày </a></div>
-                    <div><a href="#">Tuần báo khí tượng </a></div>
-                    <div><a href="#">Thông báo khí tượng tháng </a></div>
-                    <div><a href="#">Bản tin dự báo bão, Áp thấp nhiệt đới </a></div>
-                    <div><a href="#">Bản tin dự báo, cảnh báo mưa giông </a></div>
-                    <div><a href="#">Bản tin dự báo, cảnh báo mưa lớn </a></div>
-                    <div><a href="#">Bản tin dự báo, cảnh báo lũ, lũ quét và sạt lở </a></div>
-                    <div><a href="#">Bản tin dự báo, cảnh báo rét, rét đậm, rét hại </a></div>
-                </div>
-            </li>
-            <li class="hd-navigation-item float-left text-left font-weight-bold"><a href="#" class="nav-link px-1 py-0">HỒ SƠ</a>
-                <div class="sub-menu position-absolute text-left">
-                    <div><a href="#">Cấp giấy phép hoạt động dự báo, cảnh báo KTTV</a></div>
-                    <div><a href="#">Cấp phép gia hạn, sửa đổi, bổ sung giấy phép hoạt động dự báo, cảnh báo KTTV</a></div>
-                    <div><a href="#">Cấp lại giấy phép hoạt động dự báo, cảnh báo KTTV </a></div>
-                </div>
-            </li>
-            <li class="hd-navigation-item float-left text-left font-weight-bold"><a href="#" class="nav-link px-1 py-0">BIẾN ĐỔI KHÍ HẬU</a>
-                <div class="sub-menu position-absolute text-left">
-                    <div><a href="#">Kết quả đánh giá khí hậu quốc gia</a></div>
-                    <div><a href="#">Kịch bản biến đổi khí hậu chi tiết cho tỉnh Sơn La</a></div>
-                </div>
-            </li>
-            <li class="hd-navigation-item float-left text-left font-weight-bold"><a href="#" class="nav-link px-1 py-0">CHƯƠNG TRÌNH</a>
-                <div class="sub-menu position-absolute text-left">
-                    <div><a href="#">Chương trình, kế hoạch, quy hoạch về khí tượng thủy văn </a></div>
-                    <div><a href="#">Các dự án về khí tượng thủy văn </a></div>
-                </div>
-            </li>
-            <li class="hd-navigation-item float-left text-left font-weight-bold"><a href="#" class="nav-link px-1 py-0">VĂN BẢN</a>
-                <div class="sub-menu position-absolute text-left">
-                    <div><a href="#">Do Quốc hội, Chính phủ, Thủ tướng chính phủ ban hành </a></div>
-                    <div><a href="#">Do Bộ TNMT và các Bộ có liên quan ban hành </a></div>
-                    <div><a href="#">Do UBND tỉnh ban hành</a></div>
-                </div>
-            </li>
-            <li class="hd-navigation-item float-left text-left font-weight-bold">
-                <a href="#" class="nav-link px-1 py-0">BÁO CÁO</a>
-            </li>
-            <li class="hd-navigation-item float-left text-left font-weight-bold">
-                <a href="#" class="nav-link px-1 py-0">ĐĂNG KÝ</a>
-            </li>
+<nav class="navbar navbar-expand-lg navbar-dark main-navigation p-0">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#main_nav">
+    <a class="d-block"><i class="fa fa-align-justify" aria-hidden="true"></i></a>
+  </button>
+  <div class="collapse navbar-collapse" id="main_nav">
+<ul class="navbar-nav justify-content-between w-100">
+    <li class="hd-navigation-item font-weight-bold"><a href="#" class="nav-link px-2 py-0">GIỚI THIỆU</a></li>
+    <li class="hd-navigation-item nav-item font-weight-bold position-relative">
+        <a href="#" class="nav-link px-2 py-0  dropdown-toggle">QUAN TRẮC</a>
+        <ul class="dropdown-menu p-0 sub-menu rounded-0 border-0">
+            <li class="dropdown-item"><a href="{{ route('so-lieu-khi-tuong') }}">Số liệu khí tượng </a></li>
+            <li class="dropdown-item"><a href="#">Số liệu mưa </a></li>
+            <li class="dropdown-item"><a href="#">Số liệu thủy văn </a></li>
         </ul>
-    </div>
-    
+    </li>
+    <li class="hd-navigation-item nav-item font-weight-bold position-relative"><a href="#" class="nav-link px-2 py-0  dropdown-toggle">DỰ BÁO</a>
+        <ul class="dropdown-menu p-0 sub-menu rounded-0 border-0">
+            <li class="dropdown-item"><a href="#">Bản tin dự báo thời tiết, thủy văn hàng ngày </a></li>
+            <li class="dropdown-item"><a href="#">Bản tin dự báo khí tượng thủy văn 10 ngày </a></li>
+            <li class="dropdown-item"><a href="#">Tuần báo khí tượng </a></li>
+            <li class="dropdown-item"><a href="#">Thông báo khí tượng tháng </a></li>
+            <li class="dropdown-item"><a href="#">Bản tin dự báo bão, Áp thấp nhiệt đới </a></li>
+            <li class="dropdown-item"><a href="#">Bản tin dự báo, cảnh báo mưa giông </a></li>
+            <li class="dropdown-item"><a href="#">Bản tin dự báo, cảnh báo mưa lớn </a></li>
+            <li class="dropdown-item"><a href="#">Bản tin dự báo, cảnh báo lũ, lũ quét và sạt lở </a></li>
+            <li class="dropdown-item"><a href="#">Bản tin dự báo, cảnh báo rét, rét đậm, rét hại </a></li>
+        </ul>
+    </li>
+    <li class="hd-navigation-item nav-item font-weight-bold position-relative"><a href="#" class="nav-link px-2 py-0  dropdown-toggle">HỒ SƠ</a>
+        <ul class="dropdown-menu p-0 sub-menu rounded-0 border-0">
+            <li class="dropdown-item"><a href="#">Cấp giấy phép hoạt động dự báo, cảnh báo KTTV</a></li>
+            <li class="dropdown-item"><a href="#">Cấp phép gia hạn, sửa đổi, bổ sung giấy phép hoạt động dự báo, cảnh báo KTTV</a></li>
+            <li class="dropdown-item"><a href="#">Cấp lại giấy phép hoạt động dự báo, cảnh báo KTTV </a></li>
+        </ul>
+    </li>
+    <li class="hd-navigation-item nav-item font-weight-bold position-relative"><a href="#" class="nav-link px-2 py-0  dropdown-toggle">BIẾN ĐỔI KHÍ HẬU</a>
+        <ul class="dropdown-menu p-0 sub-menu rounded-0 border-0">
+            <li class="dropdown-item"><a href="#">Kết quả đánh giá khí hậu quốc gia</a></li>
+            <li class="dropdown-item"><a href="#">Kịch bản biến đổi khí hậu chi tiết cho tỉnh Sơn La</a></li>
+        </ul>
+    </li>
+    <li class="hd-navigation-item nav-item font-weight-bold position-relative"><a href="#" class="nav-link px-2 py-0  dropdown-toggle">CHƯƠNG TRÌNH</a>
+        <ul class="dropdown-menu p-0 sub-menu rounded-0 border-0">
+            <li class="dropdown-item"><a href="#">Chương trình, kế hoạch, quy hoạch về khí tượng thủy văn </a></li>
+            <li class="dropdown-item"><a href="#">Các dự án về khí tượng thủy văn </a></li>
+        </ul>
+    </li>
+    <li class="hd-navigation-item nav-item font-weight-bold position-relative"><a href="#" class="nav-link px-2 py-0  dropdown-toggle">VĂN BẢN</a>
+        <ul class="dropdown-menu p-0 sub-menu rounded-0 border-0">
+            <li class="dropdown-item"><a href="#">Do Quốc hội, Chính phủ, Thủ tướng chính phủ ban hành </a></li>
+            <li class="dropdown-item"><a href="#">Do Bộ TNMT và các Bộ có liên quan ban hành </a></li>
+            <li class="dropdown-item"><a href="#">Do UBND tỉnh ban hành</a></li>
+        </ul>
+    </li>
+    <li class="hd-navigation-item nav-item font-weight-bold position-relative">
+        <a href="#" class="nav-link px-2 py-0">BÁO CÁO</a>
+    </li>
+    <li class="hd-navigation-item nav-item font-weight-bold position-relative">
+        <a href="#" class="nav-link px-2 py-0">ĐĂNG KÝ</a>
+    </li>
+</ul>
+  </div> <!-- navbar-collapse.// -->
 </nav>
--- resources/views/layouts/navigation-water-resource.blade.php
+++ resources/views/layouts/navigation-water-resource.blade.php
@@ -1,53 +1,49 @@
-<nav class="navbar navbar-expand-lg main-navigation nav-height navbar-dark p-0">
-    <button class="navbar-toggler p-0 ml-2" data-target="#my-nav" data-toggle="collapse" aria-controls="my-nav" aria-expanded="false" aria-label="Toggle navigation">
-        <a class="d-block"><i class="fa fa-align-justify" aria-hidden="true"></i></a>
-    </button>
-    <div id="my-nav" class="collapse navbar-collapse">
-        <ul class="navbar-nav justify-content-between nav-height w-100">
-            <li class="wr-navigation-item nav-item font-weight-bold">
-                <a class="nav-link px-2 py-0" href="#">GIỚI THIỆU</a>
-            </li>
-            <li class="nav-item wr-navigation-item  font-weight-bold">
-                <a href="#" class="nav-link px-2 py-0">GIÁM SÁT</a>
-                <div class=" sub-menu position-absolute text-left">
-                    <div><a href="#">Hồ chứa thủy điện trên 2MW </a></div>
-                    <div><a href="#">Hồ chứa thủy điện dưới 2MW </a></div>
-                    <div><a href="#">Hồ chứa khai thác nước mặt sản xuất nông nghiệp </a></div>
-                    <div><a href="#">Trạm bơm </a></div>
-                    <div><a href="#">Công trình khai thác, sử dụng nước dưới đất </a></div>
-                </div>
-            </li>
-            <li class="wr-navigation-item nav-item font-weight-bold">
-                <a href="#" class="nav-link px-2 py-0">CẤP PHÉP</a>
-                <div class="sub-menu position-absolute text-left">
-                    <div><a href="#">QLCP khai thác sử dụng nước mặt </a></div>
-                    <div><a href="#">QLCP khai thác sử dụng nước dưới đất </a></div>
-                </div>
-            </li>
-            <li class="wr-navigation-item nav-item font-weight-bold">
-                <a href="#" class="nav-link px-2 py-0">THÔNG TIN DỮ LIỆU</a>
-                <div class="sub-menu position-absolute text-left">
-                    <div><a href="#">Tiểu vùng TNN </a></div>
-                    <div><a href="#">Số lượng, chất lượng nước mặt, nước dưới đất </a></div>
-                    <div><a href="#">Số liệu điều tra khảo sát địa chất thủy văn </a></div>
-                    <div><a href="#">Kết quả cấp, gia hạn, thu hồi, điều chỉnh giấy phép </a></div>
-                </div>
-            </li>
-            <li class="wr-navigation-item nav-item font-weight-bold">
-                <a href="#" class="nav-link px-2 py-0">THÔNG BÁO</a>
-                <div class="sub-menu position-absolute text-left">
-                    <div><a href="#">Tổ chức/ cá nhân </a></div>
-                    <div><a href="#">Cơ quan nhà nước  </a></div>
-                </div>
-            </li>
-            <li class="wr-navigation-item nav-item font-weight-bold">
-                <a href="#" class="nav-link px-2 py-0">BÁO CÁO</a>
-            </li>
-            <li class="wr-navigation-item nav-item font-weight-bold">
-                <a href="#" class="nav-link px-2 py-0">ĐĂNG KÝ</a>
-            </li>
+<nav class="navbar navbar-expand-lg navbar-dark main-navigation p-0">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#main_nav">
+    <a class="d-block"><i class="fa fa-align-justify" aria-hidden="true"></i></a>
+  </button>
+  <div class="collapse navbar-collapse" id="main_nav">
+<ul class="navbar-nav justify-content-between w-100">
+	<li class="wr-navigation-item nav-item font-weight-bold active"> <a class="nav-link px-2 py-0" href="#">GIỚI THIỆU </a> </li>
+    <li class="wr-navigation-item nav-item font-weight-bold dropdown position-relative">
+	    <a class="nav-link px-2 py-0  dropdown-toggle" href="#" data-toggle="dropdown">  GIÁM SÁT  </a>
+	    <ul class="dropdown-menu p-0 sub-menu rounded-0 border-0">
+            <li class="dropdown-item"><a href="#">Hồ chứa thủy điện trên 2MW </a></li>
+            <li class="dropdown-item"><a href="#">Hồ chứa thủy điện dưới 2MW </a></li>
+            <li class="dropdown-item"><a href="#">Hồ chứa khai thác nước mặt sản xuất nông nghiệp </a></li>
+            <li class="dropdown-item"><a href="#">Trạm bơm </a></li>
+            <li class="dropdown-item"><a href="#">Công trình khai thác, sử dụng nước dưới đất </a></li>
+	    </ul>
+	</li>
+    <li class="wr-navigation-item nav-item font-weight-bold position-relative">
+        <a class="nav-link px-2 py-0  dropdown-toggle" href="#" data-toggle="dropdown">CẤP PHÉP</a>
+        <ul class="dropdown-menu p-0 sub-menu rounded-0 border-0">
+            <li class="dropdown-item"><a href="#">QLCP khai thác sử dụng nước mặt </a></li>
+            <li class="dropdown-item"><a href="#">QLCP khai thác sử dụng nước dưới đất </a></li>
         </ul>
-    </div>
+    </li>
+	<li class="wr-navigation-item nav-item font-weight-bold position-relative">
+        <a href="#" class="nav-link px-2 py-0  dropdown-toggle">THÔNG TIN DỮ LIỆU</a>
+        <ul class="dropdown-menu p-0 sub-menu rounded-0 border-0">
+            <li class="dropdown-item"><a href="#">Tiểu vùng TNN </a></li>
+            <li class="dropdown-item"><a href="#">Số lượng, chất lượng nước mặt, nước dưới đất </a></li>
+            <li class="dropdown-item"><a href="#">Số liệu điều tra khảo sát địa chất thủy văn </a></li>
+            <li class="dropdown-item"><a href="#">Kết quả cấp, gia hạn, thu hồi, điều chỉnh giấy phép </a></li>
+        </ul>
+    </li>
+    <li class="wr-navigation-item nav-item font-weight-bold position-relative">
+        <a href="#" class="nav-link px-2 py-0  dropdown-toggle">THÔNG BÁO</a>
+        <ul class="dropdown-menu p-0 sub-menu rounded-0 border-0">
+            <li class="dropdown-item"><a href="#">Tổ chức/ cá nhân </a></li>
+            <li class="dropdown-item"><a href="#">Cơ quan nhà nước  </a></li>
+        </ul>
+    </li>
+    <li class="wr-navigation-item nav-item font-weight-bold">
+        <a href="#" class="nav-link px-2 py-0">BÁO CÁO</a>
+    </li>
+    <li class="wr-navigation-item nav-item font-weight-bold">
+        <a href="#" class="nav-link px-2 py-0">ĐĂNG KÝ</a>
+    </li>
+</ul>
+  </div> <!-- navbar-collapse.// -->
 </nav>
-
-<!--  -->
